5d7c654d0fcf59f3d7a3cb31eb7f8518feb87195
Paul <40543380+PauliusSasnauskas@users.noreply.github.com>
2022-01-09 18:07:47 +0200

    Add animation script

---
+++ util/animation.py
@@ -0,0 +1,9 @@
+def make_animation(input_folder, output_file, input_format="png"):
+    import ffmpeg
+    (
+        ffmpeg
+        .input('/content/' + input_folder + '/*.' + input_format, pattern_type='glob', framerate=30)
+        .output('/content/' + output_file, vcodec="mpeg4")
+        .overwrite_output()
+        .run()
+    )
