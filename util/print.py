15f8aaa738bdd2760306e45c2dcef98b9d8ffbe1
PauliusSasnauskas <40543380+PauliusSasnauskas@users.noreply.github.com>
2022-02-20 09:17:50 +0200

    Add pad3 function

--- util/print.py
+++ util/print.py
@@ -8,3 +8,8 @@ def d(expr):
 # Display number arrays unified style
 def a(arr):
     return "[" + ", ".join(f"{item:.4f}" if item < 0 else f" {item:.4f}" for item in arr) + "]"
+
+# Pad start with zeroes (3 symbols)
+def pad3(x):
+    str_x = str(x)
+    return '0' * (3 - len(str_x)) + str_x
