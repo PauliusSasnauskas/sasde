d0481efdaa59d0ae85eb239546d5fab06952165d
PauliusSasnauskas <40543380+PauliusSasnauskas@users.noreply.github.com>
2022-02-20 10:08:42 +0200

    Add logger

--- util/print.py
+++ util/print.py
@@ -1,5 +1,6 @@
 from IPython.display import display_markdown
 from sympy import latex
+import logging
 
 # Display sympy in latex style
 def d(expr):
@@ -13,3 +14,12 @@ def a(arr):
 def pad(x, n=2):
     str_x = str(x)
     return '0' * (n - len(str_x)) + str_x
+
+loggerStream = logging.StreamHandler()
+loggerStream.setFormatter(logging.Formatter("{asctime}.{msecs:0<3.0f} [{levelname}] {message}", datefmt='%H:%M:%S', style='{'))
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.handlers = [loggerStream]
+
+def info(input, *args, **kwargs):
+    logging.info(input, *args, **kwargs)
