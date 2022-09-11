6b2d0fd24cd02f21342906577c5c4ecc8d4504c0
Paulius Sasnauskas <paulius@igem.org>
2022-09-11 14:59:22 +0200

    Swap eq1 and eq2

--- eq2.py
+++ eq2.py
@@ -1,40 +1,43 @@
 import sympy as sp
 from util.interfaces import Config, EqInfo, Hyperparameters, VarInfo
+from main import run
+
+a = 1
 
 config = Config(
   eq = EqInfo(
-    name = 'y',
-    function = lambda s: s.dydx - s.k * s.x,
+    name = 'u',
+    function = lambda s: s.dudx2 + s.dudy2 + s.dudy ** 2 - 2 * s.y + s.x ** 4,
   ),
   vars = {
-    'k': VarInfo(bounds=(1.4, 1.6), integrable=True),
-    'x': VarInfo(bounds=(0, 1), integrable=False),
+    'x': VarInfo(bounds=(-1, 1), integrable=False),
+    'y': VarInfo(bounds=(-1, 1), integrable=False),
   },
   conditions = [
-    (0.001, lambda s: s.y.subs(s.x, 1) - 1)
+    (1., lambda s: s.u.subs(s.x, 0)),
+    (1., lambda s: s.u.subs(s.x, 1) - s.y - a),
+    (1., lambda s: s.u.subs(s.y, 0) - a * s.x),
+    (1., lambda s: s.u.subs(s.y, 1) - s.x * (s.x + a))
   ],
-  preoperations = [
-    lambda k, x: 0,
-    lambda k, x: 1,
-    lambda k, x: k,
-    lambda k, x: x,
-    lambda k, x: k + x,
-    lambda k, x: k * x,
-    lambda k, x: sp.exp(k),
-  ],
+  preoperations = [],
   operations = [
     lambda z: 0,
     lambda z: 1,
     lambda z: z,
     lambda z: -z,
-    lambda z: z*z,
+    lambda z: 1 + z,
+    lambda z: -z,
+    lambda z: z**2,
+    lambda z: z**3,
     lambda z: sp.exp(z) + 0,
   ],
   hyperparameters = Hyperparameters(
     lr = 0.0001,
     cellcount = 4,
   ),
-  epochs = 16,
+  epochs = 10,
   batchsize = 16,
   verbosity = 1,
 )
+
+run(config)
