6a2448718ee5912a74abb25bb08ff378a58bae4b
Paulius Sasnauskas <paulius@igem.org>
2022-09-15 16:30:51 +0200

    Update eq1

--- eq1.py
+++ eq1.py
@@ -4,23 +4,21 @@ from util.interfaces import Config, EqInfo, Hyperparameters, VarInfo
 config = Config(
   eq = EqInfo(
     name = 'y',
-    function = lambda s: s.dydx - s.k * s.x,
+    function = lambda s: s.dydx - s.k * s.y,
   ),
   vars = {
-    'k': VarInfo(bounds=(1.4, 1.6), integrable=True),
+    'k': VarInfo(bounds=(1.4, 1.6), integrable=False),
     'x': VarInfo(bounds=(0, 1), integrable=False),
   },
   conditions = [
-    (0.001, lambda s: s.y.subs(s.x, 1) - 1)
+    (2, lambda s: s.y.subs(s.x, 1) - sp.exp(1.5))
   ],
   preoperations = [
     lambda k, x: 0,
     lambda k, x: 1,
     lambda k, x: k,
     lambda k, x: x,
-    lambda k, x: k + x,
-    lambda k, x: k * x,
-    lambda k, x: sp.exp(k),
+    lambda k, x: k * x
   ],
   operations = [
     lambda z: 0,
@@ -31,10 +29,10 @@ config = Config(
     lambda z: sp.exp(z) + 0,
   ],
   hyperparameters = Hyperparameters(
-    lr = 0.0001,
+    lr = 0.003,
     cellcount = 4,
   ),
-  epochs = 16,
-  batchsize = 16,
+  epochs = 24,
+  batchsize = 32,
   verbosity = 1,
 )
